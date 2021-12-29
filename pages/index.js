44534e4e132e02bdf46482bec9b785ae4cef01a7
orsolyalukacs <orchie.lucas@gmail.com>
2021-12-28 21:22:15 -0600

    add mongo utils

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 // home page
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import Link from 'next/link'
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import Link from 'next/link';
+import clientPromise from '../lib/mongodb';
 
-export default function Home() {
+export default function Home({ isConnected }) {
   return (
     <div className={styles.container}>
       <Head>
@@ -16,6 +17,15 @@ export default function Home() {
           {/* TODO: add deployed page url*/}
           Welcome to <a href="">Friend Zones!</a>
         </h1>
+
+        {isConnected ? (
+          <h2 className="subtitle">You are connected to MongoDB</h2>
+        ) : (
+          <h2 className="subtitle">
+            You are NOT connected to MongoDB. Check the <code>README.md</code>{' '}
+            for instructions.
+          </h2>
+        )}
 
         <p className={styles.description}>
           A web app that displays a user&apos;s friends, and their associated timezones, in relation to the timezone of the user.
@@ -39,4 +49,23 @@ export default function Home() {
 
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  try {
+    // client.db() will be the default database passed in the MONGODB_URI
+    // You can change the database by calling the client.db() function and specifying a database like:
+    // const db = client.db("myDatabase");
+    // Then you can execute queries against your database like so:
+    // db.find({}) or any of the MongoDB Node Driver commands
+    await clientPromise
+    return {
+      props: { isConnected: true },
+    }
+  } catch (e) {
+    console.error(e)
+    return {
+      props: { isConnected: false },
+    }
+  }
 }
