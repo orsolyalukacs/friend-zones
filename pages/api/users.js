b362e12cbb2e219862357591bf9d50f1c7894bf5
orsolyalukacs <orchie.lucas@gmail.com>
2021-12-29 19:34:12 -0600

    add users api

---
+++ pages/api/users.js
@@ -0,0 +1,14 @@
+/* Create user endpoint */
+import { connectToDatabase } from "../../util/mongodb";
+
+export default async function handler(req, res) {
+    const { db, client } = await connectToDatabase();
+    if (req.method === "POST") {
+        const data = req.body;
+        const usersCollection = db.collection("usersCollection");
+        const result = await usersCollection.insertOne(data);
+        console.log(result);
+        client.close();
+        res.status(201).json({ message: "Data inserted successfully!" });
+    }
+}
