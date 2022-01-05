260f3b0b809a6b497ec8c9e669a61f1b9cd807ea
orsolyalukacs <orchie.lucas@gmail.com>
2022-01-05 15:19:48 -0600

    use consistent collection in db

--- pages/api/users/users.js
+++ pages/api/users/users.js
@@ -5,7 +5,7 @@ import { connectToDatabase } from "../../../util/mongodb";
 export default async function usersApi(req, res) {
     const { db } = await connectToDatabase();
     const users = await db
-        .collection("users")
+        .collection("usersCollection")
         .find({})
         .sort({ metacritic: -1 })
         .limit(20)
