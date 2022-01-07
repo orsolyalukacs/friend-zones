6ca38726dfc7b2ca5d2704765b7f4d7741ee29a3
mcclellangg <81567927+mcclellangg@users.noreply.github.com>
2022-01-07 10:51:07 -0500

    add styles for protohome

--- pages/account/ProtoHome.js
+++ pages/account/ProtoHome.js
@@ -1,14 +1,27 @@
 // friends
 // Pull in timezones from a json file and display them:
 // import
-import styles from '../../styles/Home.module.css'
+import styles from '../../styles/protohome.module.css'
 const UTC_OFFSETS = require('/data/timezones.json');
+
+function DisplayOffsets(props) {
+    const offsets = props.offsets
+
+    // Display offsets
+    const offsetDisplays = offsets.map((offset) =>
+    <div>
+        {offset}
+    </div>
+    );
+    return offsetDisplays;
+}
 
 const ProtoHome = () => {
     return (
         <div className={styles.container}>
             <main className={styles.main}>
                 <h3>Proto Home</h3>
+                <DisplayOffsets offsets={UTC_OFFSETS}></DisplayOffsets>
             </main>
         </div>
     );
