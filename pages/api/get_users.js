4ed1d7c97064bbb122fb06fc2c47c757bbfab20d
orsolyalukacs <orchie.lucas@gmail.com>
2022-01-10 21:22:22 -0600

    fix mongoose issue. rename old mongo files

--- pages/api/get_users.js
+++ pages/api/get_users.js
@@ -1,11 +1,11 @@
 /* Gets all users data from db
     http://localhost:3000/api/users/users
 */
-import { connectToDatabase } from "../../../util/mongodb";
+import { connectToDatabase } from "../../util/mongodb";
 export default async function usersApi(req, res) {
     const { db } = await connectToDatabase();
     const users = await db
-        .collection("usersCollection")
+        .collection("users")
         .find({})
         .sort({ metacritic: -1 })
         .limit(20)
--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import clientPromise from '../lib/mongodb';
 
 export default function Home({ isConnected }) {
   return (
+
     <div className={styles.container}>
       <Head>
         <title>Friend Zones app</title>
@@ -46,18 +47,12 @@ export default function Home({ isConnected }) {
           </Link>
         </div>
       </main>
-
     </div>
   )
 }
 
 export async function getServerSideProps(context) {
   try {
-    // client.db() will be the default database passed in the MONGODB_URI
-    // You can change the database by calling the client.db() function and specifying a database like:
-    // const db = client.db("myDatabase");
-    // Then you can execute queries against your database like so:
-    // db.find({}) or any of the MongoDB Node Driver commands
     await clientPromise
     return {
       props: { isConnected: true },
