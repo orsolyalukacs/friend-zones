69ab2f5573c30fb1e4f9dd069fb12ca109d102fd
mcclellangg <81567927+mcclellangg@users.noreply.github.com>
2022-01-17 11:29:58 -0500

    added friends api, and display page

--- pages/account/Dashboard.js
+++ pages/account/Dashboard.js
@@ -3,6 +3,7 @@
 // import
 import styles from '../../styles/dashboard.module.css'
 import AddFriend from '../../components/AddFriend';
+import { useState, useEffect } from 'react';
 
 const UTC_OFFSETS = require('/data/timezones.json');
 const timeSettings = { hour: '2-digit', minute: '2-digit' };
@@ -22,7 +23,50 @@ function DisplayOffsets(props) {
     );
 }
 
+function DisplayFriends(props) {
+    // Take the friends object, and display friends on a card
+    const friendList = props.friendList;
+    return (
+        <div className={styles.card}>
+            <h3>Friend List</h3>
+            {/*TODO: Ok to use friend_id? */}
+            {friendList.map((friend) => {
+                return <li key={friend._id}>{friend.name}: {friend.timezone}</li>
+            })}
+        </div>
+    )
+}
+
 const Dashboard = () => {
+    const [friendList, setFriendList] = useState([]);
+    const [newFriend, setNewFriend] = useState(false);
+
+    function handleClick() {
+        // Use to refresh friend display
+        setNewFriend(!newFriend);
+    }
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch('/api/get_friends');
+
+            if (response.status != 200) {
+                throw new Error('cannot fetch data');
+            }
+            const data = await response.json();
+            return data;
+        }
+        fetchData()
+          .then((data) => {
+              console.log('resolved', data);
+              setFriendList(data);
+          })
+          .catch((err) => {
+              console.log('rejected', err.message);
+          });
+
+    }, [newFriend]);
+
     return (
         <div>
             <div>
@@ -32,8 +76,12 @@ const Dashboard = () => {
                         <DisplayOffsets offsets={UTC_OFFSETS}></DisplayOffsets>
                     </div>
                 </div>
-                <div className={styles.card}>
-                    <AddFriend></AddFriend>
+                <div className={styles.grid}>
+                    <div className={styles.card}>
+                        <AddFriend></AddFriend>
+                        <button onClick={() => handleClick()}>Refresh Friends</button>
+                    </div>
+                    {friendList && <DisplayFriends friendList={friendList}></DisplayFriends>}
                 </div>
             </div>
         </div>
