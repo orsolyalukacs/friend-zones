352b933a84c75bc97504cc35ffd37572efc22e77
mcclellangg <81567927+mcclellangg@users.noreply.github.com>
2022-01-24 10:25:04 -0500

    added error handling, updated linting and formatting
    added error handling, updated linting and formatting
    
    updated linting and formatting rules
    
    updated linting, formatting, and fixed errors

--- pages/account/Dashboard.js
+++ pages/account/Dashboard.js
@@ -1,114 +1,113 @@
 // friends
 // Pull in timezones from a json file and display them:
 // import
-import styles from '../../styles/dashboard.module.css'
+import styles from '../../styles/dashboard.module.css';
 import AddFriend from '../../components/AddFriend';
-import { useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
+import {useState, useEffect} from 'react';
 
 const UTC_OFFSETS = require('/data/tznames.json');
-const timeSettings = { hour: '2-digit', minute: '2-digit' };
+const timeSettings = {hour: '2-digit', minute: '2-digit'};
 
 function DisplayOffsets(props) {
-    const offsets = props.offsets
-    const friendList = props.friendList
-    // Create zones of friends to check against
-    const zones = []
-    friendList.map((friend) => zones.push(friend.timezone));
-    // Display offsets
-    return (
-        offsets.map((offset) =>
-            <div className={styles.zones} key={offset.utc_offset}>
-                {offset.utc_offset}
-                <hr className={styles.line_break}></hr>
-                {(zones.includes(offset.utc_offset)) ? new Date().toLocaleTimeString([], { timeZone: (offset.timezone), ...timeSettings }) : ''}
-            </div>
-        )
-    );
+  const offsets = props.offsets;
+  const friendList = props.friendList;
+  // Create zones of friends to check against
+  const zones = [];
+  friendList.map((friend) => zones.push(friend.timezone));
+  // Display offsets
+  return (
+    offsets.map((offset) =>
+      <div className={styles.zones} key={offset.utc_offset}>
+        {offset.utc_offset}
+        <hr className={styles.line_break}></hr>
+        {(zones.includes(offset.utc_offset)) ? new Date().toLocaleTimeString([],
+            {timeZone: (offset.timezone), ...timeSettings}) : ''}
+      </div>
+    )
+  );
 }
 
 function DisplayFriends(props) {
-    // Take the friends object, and display friends on a card
-    const friendList = props.friendList;
-    const router = useRouter();
+  // Take the friends object, and display friends on a card
+  const friendList = props.friendList;
 
-    function deleteFriend(e) {
-        const target = e.target;
-        const id = target.parentNode.getAttribute('id');
-        console.log(id);
-        //const { id } = router.query  // add the id to router object
+  function deleteFriend(e) {
+    const target = e.target;
+    const id = target.parentNode.getAttribute('id');
+    console.log(id);
+    // const { id } = router.query  // add the id to router object
 
-        // Send a delete request to /api/delete_friend/id
-        fetch('/api/friends/' + id, {
-            method: 'DELETE'
-        }).then(() => alert('Friend Deleted'))
-    }
+    // Send a delete request to /api/delete_friend/id
+    fetch('/api/friends/' + id, {
+      method: 'DELETE',
+    }).then(() => alert('Friend Deleted'));
+  }
 
-    return (
-        <div className={styles.card}>
-            <h3>Friend List</h3>
-            {/*TODO: Ok to use friend_id? */}
-            {friendList.map((friend) => {
-                return (
-                    <li key={friend._id} id={friend._id}>
-                        {friend.name}: {friend.timezone}
-                        <button onClick={deleteFriend}>Delete</button>
-                    </li>
-                )
-            })}
-        </div>
-    )
+  return (
+    <div className={styles.card}>
+      <h3>Friend List</h3>
+      {/* TODO: Ok to use friend_id? */}
+      {friendList.map((friend) => {
+        return (
+          <li key={friend._id} id={friend._id}>
+            {friend.name}: {friend.timezone}
+            <button onClick={deleteFriend}>Delete</button>
+          </li>
+        );
+      })}
+    </div>
+  );
 }
 
 const Dashboard = () => {
-    const [friendList, setFriendList] = useState([]);
-    const [newFriend, setNewFriend] = useState(false);
+  const [friendList, setFriendList] = useState([]);
+  const [newFriend, setNewFriend] = useState(false);
 
-    function handleClick() {
-        // Use to refresh friend display
-        setNewFriend(!newFriend);
-    }
+  function handleClick() {
+    // Use to refresh friend display
+    setNewFriend(!newFriend);
+  }
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const response = await fetch('/api/get_friends');
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('/api/get_friends');
 
-            if (response.status != 200) {
-                throw new Error('cannot fetch data');
-            }
-            const data = await response.json();
-            return data;
-        }
-        fetchData()
-            .then((data) => {
-                console.log('resolved', data);
-                setFriendList(data);
-            })
-            .catch((err) => {
-                console.log('rejected', err.message);
-            });
+      if (response.status != 200) {
+        throw new Error('cannot fetch data');
+      }
+      const data = await response.json();
+      return data;
+    };
+    fetchData()
+        .then((data) => {
+          console.log('resolved', data);
+          setFriendList(data);
+        })
+        .catch((err) => {
+          console.log('rejected', err.message);
+        });
+  }, [newFriend]);
 
-    }, [newFriend]);
-
-    return (
-        <div>
-            <div>
-                <h3>Dashboard</h3>
-                <div className={styles.grid}>
-                    <div className={styles.zone_container}>
-                        <DisplayOffsets offsets={UTC_OFFSETS} friendList={friendList}></DisplayOffsets>
-                    </div>
-                </div>
-                <div className={styles.grid}>
-                    <div className={styles.card}>
-                        <AddFriend></AddFriend>
-                        <button onClick={() => handleClick()}>Refresh Friends</button>
-                    </div>
-                    {friendList && <DisplayFriends friendList={friendList}></DisplayFriends>}
-                </div>
-            </div>
+  return (
+    <div>
+      <div>
+        <h3>Dashboard</h3>
+        <div className={styles.grid}>
+          <div className={styles.zone_container}>
+            <DisplayOffsets offsets={UTC_OFFSETS} friendList={friendList}>
+            </DisplayOffsets>
+          </div>
         </div>
-    );
-}
+        <div className={styles.grid}>
+          <div className={styles.card}>
+            <AddFriend></AddFriend>
+            <button onClick={() => handleClick()}>Refresh Friends</button>
+          </div>
+          {friendList && <DisplayFriends friendList={friendList}></DisplayFriends>}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Dashboard;
--- pages/api/friends/[id].js
+++ pages/api/friends/[id].js
@@ -2,21 +2,23 @@
    Connect to the db, and delete a user by id
    http://localhost:3000/api/friends/id
 */
-import { connectToDatabase } from "../../../util/mongodb";
+import {connectToDatabase} from '../../../util/mongodb';
 
 export default async function handler(req, res) {
-    const ObjectId = require('mongodb').ObjectId;
-    // get request information
-    const { db } = await connectToDatabase();
-    if (req.method === "DELETE") {
-        // Get the data from the request
-        const id = req.query.id;
-        console.log('Data is: ', req.query.id);
-        const friendsCollection = db.collection('friendsCollection');
-        // Will it get id from request?
-        // May need some error handling
-        const result = await friendsCollection.deleteOne({"_id": ObjectId(id)})
-        console.log(result);
-        res.status(201).json({ message: "Data removed successfully!" });
+  const objectId = require('mongodb').ObjectId;
+  // get request information
+  const {db} = await connectToDatabase();
+  if (req.method === 'DELETE') {
+    try {
+      const id = req.query.id;
+      const friendsCollection = db.collection('friendsCollection');
+      const result = await friendsCollection.deleteOne({'_id': objectId(id)});
+      if (!result) {
+        return res.status(400).json({success: false});
+      }
+      res.status(201).json({message: 'Data removed successfully!'});
+    } catch (error) {
+      res.status(400).json({success: false});
     }
+  }
 }
