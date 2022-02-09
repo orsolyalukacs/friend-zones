588f12ddddc97a25787ec0bd3eab8a8a729f3c11
mcclellangg <81567927+mcclellangg@users.noreply.github.com>
2022-02-09 14:05:10 -0500

    added error message to NewFriend if geo api fetch is unsuccessful

--- components/NewFriend.js
+++ components/NewFriend.js
@@ -1,59 +1,62 @@
 // New Friend
-import { useRef } from "react";
+import { useRef } from 'react';
 
 const NewFriend = (props) => {
-    const nameInput = useRef(null);
-    const marker = props.marker;
+  const nameInput = useRef(null);
+  const marker = props.marker;
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-        const newFriend = {
-            _id: props.user._id,
-            name: nameInput.current.value,
-            coordinates: {
-                "latitude": marker.latitude,
-                "longitude": marker.longitude
-            },
-            timezone: props.data.timezone,
-            timezone_offset: props.data.timezone_offset
-        };
-
-        try {
-            fetch('/api/friends/create_friend', {
-                method: 'POST',
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify(newFriend)
-            }).then(() => {
-                alert(newFriend.name + ' was added!');
-                console.log('Friend added: ', newFriend);
-                props.setUpdated(!(props.updated));
-                props.setAddingFriend(false);
-                props.setMarker(null);
-            });
-        } catch (error) {
-            console.log('Failed to add Friend', error);
-        };
+    const newFriend = {
+      _id: props.user._id,
+      name: nameInput.current.value,
+      coordinates: {
+        latitude: marker.latitude,
+        longitude: marker.longitude,
+      },
+      timezone: props.data.timezone,
+      timezone_offset: props.data.timezone_offset,
     };
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <label>
-                <span>Friend&apos;s Name</span>
-                <hr></hr>
-                <input
-                    type="text"
-                    name="name"
-                    maxLength={15}
-                    size={10}
-                    ref={nameInput}>
-                </input>
-            </label>
-            <p>Lat: {marker.latitude}</p>
-            <p>Long: {marker.longitude}</p>
-            <button type="submit" value={"preventNewMarker"}>Add Friend</button>
-        </form>
-    );
+    try {
+      fetch('/api/friends/create_friend', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(newFriend),
+      }).then(() => {
+        alert(newFriend.name + ' was added!');
+        console.log('Friend added: ', newFriend);
+        props.setUpdated(!props.updated);
+        props.setAddingFriend(false);
+        props.setMarker(null);
+      });
+    } catch (error) {
+      console.log('Failed to add Friend', error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        <span>Friend&apos;s Name</span>
+        <hr></hr>
+        <input
+          type="text"
+          name="name"
+          maxLength={15}
+          size={10}
+          ref={nameInput}
+        ></input>
+      </label>
+      <p>Lat: {marker.latitude}</p>
+      <p>Long: {marker.longitude}</p>
+      {props.error && <p>Unable to get timezone info from coordinates</p>}
+      <button type="submit" value={'preventNewMarker'}>
+        Add Friend
+      </button>
+    </form>
+  );
 };
 
 export default NewFriend;
