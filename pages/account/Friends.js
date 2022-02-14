8b50d0c143a5dec3c00b5385b7742a22a5678a80
orsolyalukacs <orchie.lucas@gmail.com>
2022-02-14 16:47:55 -0600

    fix mongo connection issue for deployment. fix map rendering

--- pages/account/Friends.js
+++ pages/account/Friends.js
@@ -37,6 +37,7 @@ const Friends = () => {
         width: '80vw',
         height: '80vh',
         zoom: 2,
+        renderWorldCopies: false,
     });
 
     const mapRef = useRef();
@@ -89,12 +90,11 @@ const Friends = () => {
                 throw new Error('cannot fetch data');
             }
             const data = await response.json();
-            console.log('friends data fetched: ', data[0].friendsList);
             return data;
         };
         fetchData()
             .then((data) => {
-                // console.log('resolved', data);
+                console.log('resolved', data);
                 setFriendList(data[0].friendsList);
             })
             .catch((err) => {
